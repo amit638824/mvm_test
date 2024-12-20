d34ee856e883cbc222644fa4260d06c4e4923612
amit9792 <amit.chauhan@techwagger.com>
2024-12-20 10:09:51 +0530

    newc hanges

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
-import dotenv from "dotenv"; 
-
-const app = express();
-dotenv.config();
+import cors from "cors"; 
+const app = express(); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
