2844122840b0c266ad2455d92e32f12ee09f63f4
amit9792 <amit.chauhan@techwagger.com>
2024-12-22 16:38:45 +0530

    new vikas

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });  
 app.get("/", (req: any, res: any) => {
   // tslint:disable-next-line:no-console
-  res.send(" test Welcome to the server 2.38");
+  res.send(" test Welcome to the server 4.35...aaj");
 });
  
 app.get("/test", (req: any, res: any) => {
